e66c44dfaca87e84d30c9203053e2883f771e395
ahilash <m.abhilaash@gmail.com>
2022-08-05 16:01:29 +0200

    demo auto application with inquiry and insert services using H2 database

--- auto/src/main/java/com/temp/auto/auto/AutoApplication.java
+++ auto/src/main/java/com/temp/auto/auto/AutoApplication.java
@@ -2,8 +2,10 @@ package com.temp.auto.auto;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackageClasses = {SwaggerConfig.class})
 public class AutoApplication {
 
 	public static void main(String[] args) {
